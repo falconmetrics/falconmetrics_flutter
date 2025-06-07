fa526e3d9b0f320492c698e6114163d23c424173
Remon <remonhelmond+dev@gmail.com>
2025-06-07 17:50:20 +0100

    revert deployment target

--- ios/falconmetrics_flutter/Package.swift
+++ ios/falconmetrics_flutter/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "falconmetrics_flutter",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "falconmetrics-flutter", targets: ["falconmetrics_flutter"])
